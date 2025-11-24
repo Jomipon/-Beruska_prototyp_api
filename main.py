858f2d03459665761f9db4574a67f8334bd83e58
Jomipon <tomas.vlasaty@centrum.cz>
2025-11-24 22:37:35 +0100

    post nejede

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET","POST","PUT"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
